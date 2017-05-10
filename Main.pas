4bc44ca9ff30f58973818e960c70dddbf5aa8127
luxidea <contact@luxidea.net>
2017-05-11 02:58:22 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -197,7 +197,7 @@ begin
      end;
 end;
 
-procedure TForm1.DrawModel;                                                     { OpenGL 2.1 - GLSL 1.2 }
+procedure TForm1.DrawModel;                                                     { OpenGL 3.0 - GLSL 1.3 }
 begin
      with _Arra do
      begin
