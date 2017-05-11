3b32fc610c55b3e7c69cc09a0f2591d51bb9b55c
luxidea <contact@luxidea.net>
2017-05-11 23:09:36 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -9,15 +9,15 @@ uses
   Winapi.OpenGL, Winapi.OpenGLext,
   System.UITypes,
   Vcl.ExtCtrls,
-  LUX, LUX.D3, LUX.GPU.OpenGL.Shader, LUX.GPU.OpenGL.GLView;
+  LUX, LUX.D3, LUX.GPU.OpenGL.Buffer, LUX.GPU.OpenGL.GLView;
 
 type
   TForm1 = class(TForm)
     Panel1: TPanel;
       GLView1: TGLView;
       GLView2: TGLView;
-      GLView3: TGLView;
-      GLView4: TGLView;
+    GLView3: TGLView;
+    GLView4: TGLView;
     Timer1: TTimer;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -27,9 +27,9 @@ type
     _Angle :Single;
   public
     { Public 宣言 }
-    _BufV :TGLBuffer<TSingle3D>;
-    _BufC :TGLBuffer<TAlphaColorF>;
-    _BufF :TGLBuffer<Cardinal>;
+    _BufV :TGLBufferV<TSingle3D>;
+    _BufC :TGLBufferV<TAlphaColorF>;
+    _BufF :TGLBufferI<TCardinal3D>;
     ///// メソッド
     procedure MakeModel;
     procedure DrawModel;
@@ -50,28 +50,28 @@ implementation //###############################################################
 
 procedure TForm1.MakeModel;
 const
-     Ps :array [ 0..7 ] of TSingle3D = ( ( X:-1; Y:-1; Z:-1 ),
-                                         ( X:+1; Y:-1; Z:-1 ),
-                                         ( X:-1; Y:+1; Z:-1 ),
-                                         ( X:+1; Y:+1; Z:-1 ),
-                                         ( X:-1; Y:-1; Z:+1 ),
-                                         ( X:+1; Y:-1; Z:+1 ),
-                                         ( X:-1; Y:+1; Z:+1 ),
-                                         ( X:+1; Y:+1; Z:+1 ) );
-     Cs :array [ 0..7 ] of TAlphaColorF = ( ( R:0; G:0; B:0; A:1 ),
-                                            ( R:1; G:0; B:0; A:1 ),
-                                            ( R:0; G:1; B:0; A:1 ),
-                                            ( R:1; G:1; B:0; A:1 ),
-                                            ( R:0; G:0; B:1; A:1 ),
-                                            ( R:1; G:0; B:1; A:1 ),
-                                            ( R:0; G:1; B:1; A:1 ),
-                                            ( R:1; G:1; B:1; A:1 ) );
-     Fs :array [ 0..11, 0..2 ] of Cardinal = ( ( 0, 4, 6 ), ( 6, 2, 0 ),
-                                               ( 0, 1, 5 ), ( 5, 4, 0 ),
-                                               ( 0, 2, 3 ), ( 3, 1, 0 ),
-                                               ( 7, 5, 1 ), ( 1, 3, 7 ),
-                                               ( 7, 3, 2 ), ( 2, 6, 7 ),
-                                               ( 7, 6, 4 ), ( 4, 5, 7 ) );
+     Ps :array [ 0..8-1 ] of TSingle3D = ( ( X:-1; Y:-1; Z:-1 ),
+                                           ( X:+1; Y:-1; Z:-1 ),
+                                           ( X:-1; Y:+1; Z:-1 ),
+                                           ( X:+1; Y:+1; Z:-1 ),
+                                           ( X:-1; Y:-1; Z:+1 ),
+                                           ( X:+1; Y:-1; Z:+1 ),
+                                           ( X:-1; Y:+1; Z:+1 ),
+                                           ( X:+1; Y:+1; Z:+1 ) );
+     Cs :array [ 0..8-1 ] of TAlphaColorF = ( ( R:0; G:0; B:0; A:1 ),
+                                              ( R:1; G:0; B:0; A:1 ),
+                                              ( R:0; G:1; B:0; A:1 ),
+                                              ( R:1; G:1; B:0; A:1 ),
+                                              ( R:0; G:0; B:1; A:1 ),
+                                              ( R:1; G:0; B:1; A:1 ),
+                                              ( R:0; G:1; B:1; A:1 ),
+                                              ( R:1; G:1; B:1; A:1 ) );
+     Fs :array [ 0..12-1 ] of TCardinal3D = ( ( _1:0; _2:4; _3:6 ), ( _1:6; _2:2; _3:0 ),
+                                              ( _1:0; _2:1; _3:5 ), ( _1:5; _2:4; _3:0 ),
+                                              ( _1:0; _2:2; _3:3 ), ( _1:3; _2:1; _3:0 ),
+                                              ( _1:7; _2:5; _3:1 ), ( _1:1; _2:3; _3:7 ),
+                                              ( _1:7; _2:3; _3:2 ), ( _1:2; _2:6; _3:7 ),
+                                              ( _1:7; _2:6; _3:4 ), ( _1:4; _2:5; _3:7 ) );
 begin
      //    2-------3
      //   /|      /|
@@ -81,35 +81,15 @@ begin
      //  |/      |/
      //  4-------5
 
-     with _BufV do
-     begin
-          Count := 8;
+     glEnableClientState( GL_VERTEX_ARRAY );
+     glEnableClientState( GL_COLOR_ARRAY  );
 
-          Bind;
-            glBufferData( GL_ARRAY_BUFFER, SizeOf( Ps ), @Ps[ 0 ], GL_STATIC_DRAW );
-          Unbind;
-     end;
-
-     with _BufC do
-     begin
-          Count := 8;
-
-          Bind;
-            glBufferData( GL_ARRAY_BUFFER, SizeOf( Cs ), @Cs[ 0 ], GL_STATIC_DRAW );
-          Unbind;
-     end;
-
-     with _BufF do
-     begin
-          Count := 36;
-
-          Bind;
-            glBufferData( GL_ELEMENT_ARRAY_BUFFER, SizeOf( Fs ), @Fs[ 0, 0 ], GL_STATIC_DRAW );
-          Unbind;
-     end;
+     _BufV.Import( Ps );
+     _BufC.Import( Cs );
+     _BufF.Import( Fs );
 end;
 
-procedure TForm1.DrawModel;                                                     { OpenGL 1.5 (2003) }
+procedure TForm1.DrawModel;
 begin
      with _BufV do
      begin
@@ -139,12 +119,9 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
      _Angle := 0;
 
-     glEnableClientState( GL_VERTEX_ARRAY );
-     glEnableClientState( GL_COLOR_ARRAY  );
-
-     _BufV := TGLBuffer<TSingle3D>   .Create( GL_ARRAY_BUFFER         );
-     _BufC := TGLBuffer<TAlphaColorF>.Create( GL_ARRAY_BUFFER         );
-     _BufF := TGLBuffer<Cardinal>    .Create( GL_ELEMENT_ARRAY_BUFFER );
+     _BufV := TGLBufferV<TSingle3D>   .Create;
+     _BufC := TGLBufferV<TAlphaColorF>.Create;
+     _BufF := TGLBufferI<TCardinal3D> .Create;
 
      MakeModel;
 
